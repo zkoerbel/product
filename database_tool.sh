d33e882766c98257fcf2fecf0a31432947140b86
zkoerbel <zachary.koerbel@gmail.com>
2013-07-09 22:45:05 -0400

    Play Setup With Eclipse
    Play version 2.1.2 with java, the project is called code and it is
    setup to be imported into eclipse (click "import existing projects from
    workspace" and use the /product/code directory to have access to it)

--- database_tool.sh
+++ database_tool.sh
@@ -6,6 +6,10 @@ mkdir DatabaseScripts/$date
 
 filename=DatabaseScripts/$date/$1.out
 
-mysql -h web373.webfaction.com -u product_admin -p --verbose < $1 > $filename
+echo "Enter ssh password"
+ssh zkoerbel@web373.webfaction.com
+
+echo "Enter mysql password"
+mysql -u product_admin -p --verbose < $1 > $filename
 
 mv $1  DatabaseScripts/$date/$1
